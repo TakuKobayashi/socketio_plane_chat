7e7c3fb100d7a3562ca25db0f9458de95b93420e
小林拓 <keep_slimbody@yahoo.co.jp>
2015-06-01 00:43:56 +0900

    socketio connectionしてemit()ーバーに送ったもの)を受け取れるようにした

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
+  socket.on('message', function(msg){
+    console.log('message: ' + msg);
+  });
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
