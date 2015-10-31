3a9671e345e369d1b2004cbb5b6e74c9a701bd00
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-11-01 01:37:46 +0900

    twitterのタイムラインと統合しちゃった

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 var express = require('express');
 var app = express();
+
+var MeCab = new require('mecab-async'), mecab = new MeCab();
+var twitter = require('twitter');   // twitterモジュールを読み込み
+var fs = require('fs');
+var mysql = require('mysql');
+var moment = require("moment");
+
+var twitterInfo = JSON.parse(fs.readFileSync('./config/twitter.json', 'utf8'));
+var mysqlInfo = JSON.parse(fs.readFileSync('./config/mysql.json', 'utf8'));
+// アプリ登録時に取得したkeyを入れて初期化
+var client = new twitter(twitterInfo);
+
+var connection = mysql.createConnection(mysqlInfo);
+connection.connect();
+
 //サーバーの立ち上げ
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
@@ -12,6 +27,46 @@ app.get('/', function(req, res){
 
 //サーバーと接続されると呼ばれる
 io.on('connection', function(socket){
+  // タイムラインから、自分のアカウント名を含む文字列でフィルターする
+  client.stream('statuses/filter', {track: 'mbs'}, function(stream) {
+    stream.on('data', function(tweet) {
+      mecab.parse(tweet.text, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      });
+      var params = {
+        tweet: tweet.text,
+        twitter_id: tweet.user.id,
+        user_name: tweet.user.name,
+        tweeted_at: moment(tweet.user.created_at).format('YYYY-MM-DD HH:mm:ss'),
+        profile_image_url: tweet.user.profile_image_url,
+        profile_background_image_url: tweet.user.profile_background_image_url
+      };
+      var query = "INSERT INTO `tweets` (";
+      var keys = [];
+      var values = [];
+      Object.keys(params).forEach(function (key) {
+        keys.push(key);
+        values.push(params[key]);
+      });
+      query += keys.join(",");
+      query += ") VALUES ('";
+      query += values.join("','");
+      query += "')";
+      connection.query(query, function(err, rows, fields) {
+        if (err) throw err;
+        console.log('The solution is: ', rows);
+      });
+
+      io.emit('tweetInfo', params);
+      console.log(tweet);
+    });
+ 
+    stream.on('error', function(error) {
+      throw error;
+    });
+  });
+
   console.log('a user connected');
   //接続している、人達(socket)がサーバーにメッセーッジを送った時にcallbackされるイベントを登録
   //第一引数はイベント名
