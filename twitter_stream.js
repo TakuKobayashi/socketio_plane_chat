e784ddaccde46fc7143ec79bd7c2631144a68ffd
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-10-31 22:29:49 +0900

    変なものが入っていた

--- twitter_stream.js
+++ twitter_stream.js
@@ -2,7 +2,7 @@ var MeCab = new require('mecab-async'), mecab = new MeCab();
 var twitter = require('twitter');   // twitterモジュールを読み込み
 
 // アプリ登録時に取得したkeyを入れて初期化
-var client = new twitter(require('./config/twitter.json'););
+var client = new twitter(require('./config/twitter.json'));
 
 // タイムラインから、自分のアカウント名を含む文字列でフィルターする
 client.stream('statuses/filter', {track: 'mbs'}, function(stream) {
