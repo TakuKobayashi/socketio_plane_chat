7498ea984144539daae4d9ee579a3b81fd0ffce0
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-10-31 22:27:40 +0900

    mecab入れちゃった

--- twitter_stream.js
+++ twitter_stream.js
@@ -1,3 +1,4 @@
+var MeCab = new require('mecab-async'), mecab = new MeCab();
 var twitter = require('twitter');   // twitterモジュールを読み込み
 
 // アプリ登録時に取得したkeyを入れて初期化
@@ -6,6 +7,11 @@ var client = new twitter(require('./config/twitter.json'););
 // タイムラインから、自分のアカウント名を含む文字列でフィルターする
 client.stream('statuses/filter', {track: 'mbs'}, function(stream) {
   stream.on('data', function(tweet) {
+    mecab.parse(tweet.text, function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+
     console.log(tweet);
   });
  
