a9a8da4246cbdfe133b8aa990cefe452ce99035c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-10-31 22:37:14 +0900

    設定ファイルがうまく読み込めていなかった

--- twitter_stream.js
+++ twitter_stream.js
@@ -1,8 +1,10 @@
 var MeCab = new require('mecab-async'), mecab = new MeCab();
 var twitter = require('twitter');   // twitterモジュールを読み込み
+var fs = require('fs');
 
+var twitterInfo = JSON.parse(fs.readFileSync('./config/twitter.json', 'utf8'));
 // アプリ登録時に取得したkeyを入れて初期化
-var client = new twitter(require('./config/twitter.json'));
+var client = new twitter(twitterInfo);
 
 // タイムラインから、自分のアカウント名を含む文字列でフィルターする
 client.stream('statuses/filter', {track: 'mbs'}, function(stream) {
