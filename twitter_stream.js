f20d27d54ab4caacc5d58864f8336c798578c8d4
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-10-31 22:21:46 +0900

    twitterのinfo情報追加+ignoreに追加

--- twitter_stream.js
+++ twitter_stream.js
@@ -1,12 +1,7 @@
 var twitter = require('twitter');   // twitterモジュールを読み込み
 
 // アプリ登録時に取得したkeyを入れて初期化
-var client = new twitter({
-    consumer_key: '-----',
-    consumer_secret: '------',
-    access_token_key: '------',
-    access_token_secret: '------'
-});
+var client = new twitter(require('./config/twitter.json'););
 
 // タイムラインから、自分のアカウント名を含む文字列でフィルターする
 client.stream('statuses/filter', {track: 'mbs'}, function(stream) {
