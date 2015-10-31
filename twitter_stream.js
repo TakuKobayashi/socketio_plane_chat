f8affe58f04664ccd442e95ce9e59ce5b1c09bd4
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-10-31 23:21:01 +0900

    mysqlも入れちゃった

--- twitter_stream.js
+++ twitter_stream.js
@@ -1,10 +1,16 @@
 var MeCab = new require('mecab-async'), mecab = new MeCab();
 var twitter = require('twitter');   // twitterモジュールを読み込み
 var fs = require('fs');
+var mysql = require('mysql');
 
 var twitterInfo = JSON.parse(fs.readFileSync('./config/twitter.json', 'utf8'));
+var mysqlInfo = JSON.parse(fs.readFileSync('./config/mysql.json', 'utf8'));
 // アプリ登録時に取得したkeyを入れて初期化
 var client = new twitter(twitterInfo);
+
+var connection = mysql.createConnection(mysqlInfo);
+connection.connect();
+
 
 // タイムラインから、自分のアカウント名を含む文字列でフィルターする
 client.stream('statuses/filter', {track: 'mbs'}, function(stream) {
@@ -20,4 +26,10 @@ client.stream('statuses/filter', {track: 'mbs'}, function(stream) {
   stream.on('error', function(error) {
     throw error;
   });
+});
+
+connection.query('SELECT * from tweets;', function(err, rows, fields) {
+  if (err) throw err;
+  console.log('The solution is: ', rows);
+  console.log('The solution is: ', fields);
 });
